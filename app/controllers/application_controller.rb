93c2ba3acd8dae1a2f29e6c575a0ba0de87a0ac0
shylabo <shuya.sohot@gmail.com>
2023-12-24 00:28:35 -0800

    Expose Doorkeeper user to JR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  include JSONAPI::ActsAsResourceController
+  skip_before_action :verify_authenticity_token
+
+  private
+
+  def context
+    {current_user: current_user}
+  end
+
+  def current_user
+    @current_user ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
---
+++ app/resources/application_resource.rb
@@ -0,0 +1,17 @@
+class ApplicationResource < JSONAPI::Resource
+  abstract
+
+  private
+
+  def current_user
+    context.fetch(:current_user)
+  end
+
+  class << self
+    private
+
+    def current_user(options)
+      options.fetch(:context).fetch(:current_user)
+    end
+  end
+end
